ba6e0b9a3687d787730cd3ed240236325f5d150c
LukaProtzer <lukarudi.protzer@gmail.com>
2026-07-23 10:21:52 +0200

    Shopping Mehrlisten-Ansicht aktivieren

--- lib/features/main/main_screen.dart
+++ lib/features/main/main_screen.dart
@@ -4,7 +4,7 @@ import '../home/home_screen.dart';
 import '../household/household_screen.dart';
 import '../inventory/inventory_screen.dart';
 import '../settings/settings_screen.dart';
-import '../shopping/shopping_screen.dart';
+import '../shopping/shopping_lists_screen.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -18,7 +18,7 @@ class _MainScreenState extends State<MainScreen> {
 
   final List<Widget> _pages = const [
     HomeScreen(),
-    ShoppingScreen(),
+    ShoppingListsScreen(),
     InventoryScreen(),
     HouseholdScreen(),
     SettingsScreen(),
@@ -42,27 +42,27 @@ class _MainScreenState extends State<MainScreen> {
           NavigationDestination(
             icon: Icon(Icons.home_outlined),
             selectedIcon: Icon(Icons.home),
-            label: "Home",
+            label: 'Home',
           ),
           NavigationDestination(
             icon: Icon(Icons.shopping_cart_outlined),
             selectedIcon: Icon(Icons.shopping_cart),
-            label: "Einkauf",
+            label: 'Einkauf',
           ),
           NavigationDestination(
             icon: Icon(Icons.inventory_2_outlined),
             selectedIcon: Icon(Icons.inventory_2),
-            label: "Inventar",
+            label: 'Inventar',
           ),
           NavigationDestination(
             icon: Icon(Icons.people_outline),
             selectedIcon: Icon(Icons.people),
-            label: "Haushalt",
+            label: 'Haushalt',
           ),
           NavigationDestination(
             icon: Icon(Icons.settings_outlined),
             selectedIcon: Icon(Icons.settings),
-            label: "Mehr",
+            label: 'Mehr',
           ),
         ],
       ),
